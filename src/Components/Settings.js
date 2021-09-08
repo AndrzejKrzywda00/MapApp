63fdfaffd581a557c88200a8babc25208692e9e9
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2021-09-08 12:06:56 +0200

    choosing a tool has a working base now

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -5,14 +5,59 @@ import DataOutput from "./DataOutput";
 import PathFindingMainSettings from "./PathFindingMainSettings";
 
 class Settings extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            erase: false,
+            wall: false,
+            target: true
+        }
+        this.handleTool = this.handleTool.bind(this);
+    }
+
+    handleTool(tool) {
+
+        switch(tool) {
+
+            case "erase":
+                if(this.state.erase === false) {
+                    this.setState({erase: true});
+                    this.setState({wall: false});
+                    this.setState({target: false});
+                }
+                break;
+            case "wall":
+                if(this.state.wall === false) {
+                    this.setState({erase: false});
+                    this.setState({wall: true});
+                    this.setState({target: false});
+                }
+                break;
+            case "target":
+                if(this.state.target === false) {
+                    this.setState({erase: false});
+                    this.setState({wall: false});
+                    this.setState({target: true});
+                }
+                break;
+        }
+
+    }
+
+
     render() {
+
+        const {erase,wall,target} = this.state;
+
         return (
             <div className={"options"}>
                 <div className={"column"}>
                     <PathFindingMainSettings/>
                 </div>
                 <div className={"column"}>
-                    <ToolSettings/>
+                    <ToolSettings handleTool={this.handleTool} erase={erase} wall={wall} target={target}/>
                 </div>
                 <div className={"column"}>
                     <DataOutput/>
--- src/Components/ToolSettings.js
+++ src/Components/ToolSettings.js
@@ -9,28 +9,41 @@ class ToolSettings extends Component {
 
     constructor(props) {
         super(props);
+
+        this.handleClickErase = this.handleClickErase.bind(this);
+        this.handleClickTarget = this.handleClickTarget.bind(this);
+        this.handleClickWall = this.handleClickWall.bind(this);
     }
 
     handleClickErase() {
-
+        let tool = "erase";
+        this.props.handleTool(tool);
     }
 
     handleClickWall() {
-
+        let tool = "wall";
+        this.props.handleTool(tool);
     }
 
     handleClickTarget() {
-
+        let tool = "target";
+        this.props.handleTool(tool);
     }
 
     render() {
+
+        const TYPES = ["tool-button","tool-button-clicked"];
+        let buttonTypeErase = this.props.erase ? TYPES[1] : TYPES[0];
+        let buttonTypeWall = this.props.wall ? TYPES[1] : TYPES[0];
+        let buttonTypeTarget = this.props.target ? TYPES[1] : TYPES[0];
+
         return (
             <div>
                 <h1>Edit the grid with chosen tool</h1>
                 <ButtonGroup>
-                    <Button id={"tool-button"} onClick={this.handleClickErase}><img src={eraser} alt={""} id={"img-tool"}/>Erase</Button>
-                    <Button id={"tool-button"} onClick={this.handleClickWall}><img src={wall} alt={""} id={"img-tool"}/>Wall</Button>
-                    <Button id={"tool-button"} onClick={this.handleClickTarget}><img src={pencil} alt={""} id={"img-tool"}/>Target</Button>
+                    <Button id={buttonTypeErase} onClick={this.handleClickErase}><img src={eraser} alt={""} id={"img-tool"}/>Erase</Button>
+                    <Button id={buttonTypeWall} onClick={this.handleClickWall}><img src={wall} alt={""} id={"img-tool"}/>Wall</Button>
+                    <Button id={buttonTypeTarget} onClick={this.handleClickTarget}><img src={pencil} alt={""} id={"img-tool"}/>Target</Button>
                 </ButtonGroup>
             </div>
         );
